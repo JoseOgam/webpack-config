eb227f84f0feaa7ce9c6479ac1fb10c7723fa627
JoseOgam <jotienoochieng@gmail.com>
2020-07-11 14:03:17 -0400

    setting up webpack dev-server and webpack dev-tool

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 class Web extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    };
+  }
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <List />
       </div>
     );
   }
 }
+const List = () => {
+  return (
+    <div>
+      <h1>Hello React</h1>
+    </div>
+  );
+};
 
 ReactDOM.render(<Web />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
-var path = require("path");
+var path = require('path')
 
 module.exports = {
-  entry: "./src/app.js",
+  entry: './src/app.js',
   output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
-        loader: "babel-loader",
+        loader: 'babel-loader',
         test: /\.js$/,
-        exclude: /node_modules/,
-      },
-    ],
+        exclude: /node_modules/
+      }
+    ]
   },
-};
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+}
