5d6df81a44838f4ad5c007672a33df18301db308
JoseOgam <jotienoochieng@gmail.com>
2020-07-11 10:49:37 -0400

    run babel to convert jsx to js elements

---
+++ src/app.js
@@ -0,0 +1,11 @@
+class Web extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Web />, document.getElementById("app"));
